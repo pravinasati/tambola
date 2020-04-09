4b5e3d803aca2587a83ee4fa05ae7cafac1b5e04
Yash <yashsugandh26@gmail.com>
2020-04-09 18:46:51 +0530

    Adding project files

---
+++ src/main/java/com/innovatie/tambola/TambolaApplication.java
@@ -0,0 +1,13 @@
+package com.innovatie.tambola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TambolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TambolaApplication.class, args);
+	}
+
+}
